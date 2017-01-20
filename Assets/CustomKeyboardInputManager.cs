2b56dce3e3bd47ca76d368386d424f24ba8e242a
AskoL <metabug@gmail.com>
2017-01-20 23:32:51 +0200

    Added input manager. WIP

---
+++ Assets/CustomKeyboardInputManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomKeyboardInputManager : MonoBehaviour {
+
+    [SerializeField]
+    public string _player1Left;
+    [SerializeField]
+    public string _player1Right;
+    [SerializeField]
+    public string _player1Action;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/PlayerBehavior.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class PlayerBehavior : MonoBehaviour {
+
+    [SerializeField]
+    private TeamUtility.IO.PlayerID _playerID;
+    [SerializeField]
+    private float _speed;
+    [SerializeField]
+    private KeyCode _upKey;
+    [SerializeField]
+    private KeyCode _downKey;
+    [SerializeField]
+    private KeyCode _leftKey;
+    [SerializeField]
+    private KeyCode _rightKey;
+    [SerializeField]
+    private KeyCode _actionKey;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    // Update is called once per frame
+    private void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        float horDir = 0f;
+        float verDir = 0f;
+        if (Input.GetKey(_leftKey))
+        {
+            horDir = -1;
+        }
+        else if (Input.GetKey(_rightKey))
+        {
+            horDir = 1;
+        }
+        else
+        {
+            horDir = TeamUtility.IO.InputManager.GetAxis("Left Stick Horizontal", _playerID);
+        }
+
+        if (Input.GetKey(_downKey))
+        {
+            verDir = -1;
+        }
+        else if (Input.GetKey(_upKey))
+        {
+            verDir = 1;
+        }
+        else
+        {
+            TeamUtility.IO.InputManager.GetAxis("Left Stick Vertical", _playerID);
+        }
+
+        Vector3 moveVector = new Vector3(0, 0, verDir);
+        transform.Translate(moveVector.normalized * _speed * Time.deltaTime);
+        transform.Rotate(0, horDir*10, 0);
+
+    }
+}
