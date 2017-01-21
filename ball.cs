3c0018095153f582d9046d78961764621caf8716
FroznShadow <dark.ace90@gmail.com>
2017-01-21 14:12:22 +0200

    IdleAnimation and ball script added

---
+++ ball.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ball : MonoBehaviour {
+    private Vector3 startLocation;
+    public int player1Score;
+    public int player2Score;
+    
+	// Use this for initialization
+	void Start () {
+        startLocation = gameObject.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if(transform.position.y < -50)
+        {
+            addScore();returnToStartLocation();
+        }
+	}
+    void returnToStartLocation()
+    {
+        gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        gameObject.transform.position = startLocation;
+       
+    }
+    void addScore()
+    {
+        if (transform.position.z < startLocation.z)
+        {
+            player1Score++;
+
+        }
+        else player2Score++;
+    }
+
+}
